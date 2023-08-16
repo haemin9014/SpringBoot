4d1261e90f247a20a8e1976af7e1ec518ef777f5
haemin9014 <leehaemin9014@gmail.com>
2023-08-15 21:36:01 -0700

    starting spring boot - basic

---
+++ HelloSpringBoot/hellospringboot/src/test/java/hellospringboot/example/hellospringboot/HellospringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package hellospringboot.example.hellospringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellospringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
